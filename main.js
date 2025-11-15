27b611a11d8a904921890ab1e588a0b71a372893
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-11-15 18:29:39 +0530

    validate user entered category

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ import {
   selectCategoryMap,
 } from "./src/filter-pokemons/filter_pokemons.js";
 
-const getOptionInput = (categoryOptions, category) => {
+const isValidCategory = (userInput) => {
+  return ["type", "weakness"].includes(userInput);
+};
+
+const isValidOption = (userInput, categoryOptions) => {
+  return Object.keys(categoryOptions).some((key) => key === userInput);
+};
+
+const getOptionInput = (categoryOptions, category, optionMsg) => {
   console.log(
     `\nFilter Pokemons By ${category[0].toUpperCase() + category.slice(1)}`,
   );
@@ -11,20 +19,31 @@ const getOptionInput = (categoryOptions, category) => {
   for (const tag in categoryOptions) {
     console.log(`${tag}: ${categoryOptions[tag]}`);
   }
-  return prompt("Enter Option: ");
+  return prompt(optionMsg);
 };
 
-const getFilterCategory = () => {
+const getFilterCategory = (message) => {
   console.log(`Type\nWeakness\n`);
 
-  return prompt("Enter Category: ");
+  return prompt(message);
 };
 
-const getFilterInputs = () => {
-  const category = getFilterCategory().toLowerCase();
-  const categoryOptions = selectCategoryMap(category);
-  const userInput = getOptionInput(categoryOptions, category);
-  return { category, userInput };
+const getFilterInputs = (
+  categoryMsg = "Enter Category: ",
+  optionMsg = "Enter Option: ",
+) => {
+  const category = getFilterCategory(categoryMsg).toLowerCase();
+
+  if (isValidCategory(category)) {
+    const categoryOptions = selectCategoryMap(category);
+    const userInput = getOptionInput(categoryOptions, category, optionMsg);
+
+    if (isValidOption(userInput, categoryOptions)) {
+      return { category, userInput };
+    }
+    
+  }
+  return getFilterInputs("Enter Valid Category: ", optionMsg);
 };
 
 const main = () => {

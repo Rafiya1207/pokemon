086e7dcc7db77d24a9bedbca6694caf0e00df4d9
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-11-15 17:21:19 +0530

    export sort function

--- src/sort/sort_pokemons.js
+++ src/sort/sort_pokemons.js
@@ -1,11 +1,7 @@
-import { pokemons } from "../pokedox.js";
-
 const parseID = (id) => parseInt(id.slice(1));
 
-const sortByID = (pokemons) =>
+export const sortByID = (pokemons) =>
   pokemons
     .map((pokemon) => parseID(pokemon.id))
     .sort((a, b) => a - b)
     .map((currID) => pokemons.find(({ id }) => id.includes(currID)));
-
-sortByID(pokemons);

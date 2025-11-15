43b4a0d3ccfb921c01eb783212e2fab31b0120f7
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-11-15 16:53:03 +0530

    git move filter files to new sub directory

---
+++ src/filter-pokemons/filter_category_maps.js
@@ -0,0 +1,28 @@
+export const typeMap = {
+  f: "Fire",
+  g: "Grass",
+  n: "Normal",
+  fl: "Flying",
+  p: "Poison",
+  w: "Water",
+  b: "Bug",
+  s: "Steel",
+  e: "Electric",
+};
+
+export const weaknessMap = {
+  f: "Fire",
+  fl: "Flying",
+  w: "Water",
+  e: "Electric",
+  r: "Rock",
+  i: "Ice",
+  fa: "Fair",
+  p: "Poison",
+  s: "Steel",
+  py: "Psyhic",
+  b: "Bug",
+  g: "Grass",
+  gr: "Ground",
+  ft: "Fighting",
+};
---
+++ src/filter-pokemons/filter_pokemons.js
@@ -0,0 +1,19 @@
+import { typeMap, weaknessMap } from "./filter_category_maps.js";
+import { pokemons } from "../pokedox.js";
+
+export const selectCategoryMap = (category) => {
+  const categoryMaps = {
+    "weakness": weaknessMap,
+    "type": typeMap,
+  };
+  return categoryMaps[category];
+};
+
+export const filterByCategory = (category, criteria) =>
+  pokemons
+    .filter((pokemon) => pokemon[category].includes(criteria));
+
+export const filterPokemons = (category, tag) => {
+  const categoryMap = selectCategoryMap(category);
+  return filterByCategory(category, categoryMap[tag].toLowerCase());
+};

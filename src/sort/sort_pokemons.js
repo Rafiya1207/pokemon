1addff5eedc4e1f5f5087ad4f44cac5033c997e3
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-11-15 17:25:11 +0530

    destruct id in sort function

--- src/sort/sort_pokemons.js
+++ src/sort/sort_pokemons.js
@@ -2,6 +2,6 @@ const parseID = (id) => parseInt(id.slice(1));
 
 export const sortByID = (pokemons) =>
   pokemons
-    .map((pokemon) => parseID(pokemon.id))
+    .map(({ id }) => parseID(id))
     .sort((a, b) => a - b)
     .map((currID) => pokemons.find(({ id }) => id.includes(currID)));
